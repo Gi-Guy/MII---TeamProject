f9b05b58e43f34994eb7bdcba434d13e47108c8b
Guy <Guy9502@Gmail.com>
2025-03-02 19:50:54 +0200

    Users Controller

--- Controllers/userController.ts
+++ Controllers/userController.ts
@@ -1,7 +1,20 @@
 import { User } from "../Models/User";
 
 export class UserController {
-    private users: User[] = [];
+    private static users: User[] = [];
+    private static admins: User[] = [];
 
+    private static saveUsers(): void{
+        const allUser = [...UserController.users, ...UserController.admins];
+        localStorage.setItem("users", JSON.stringify(allUser));
+    }
+
+    static loadUsers(): void{
+        const allUsers = localStorage.getItem("users");
+        const userArray = allUsers ? JSON.parse(allUsers) : [];
+
+        UserController.users = userArray.filter((user: User) => !user.isAdmin);
+        UserController.admins = userArray.filter((user: User) => user.isAdmin);
+    }
 }
 
