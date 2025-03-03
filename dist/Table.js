19def976f696ac3f0c060802ae929ac8e91256f8
YaelMeydan <yael.meydan@gmail.com>
2025-03-04 00:44:10 +0200

    tables-need to update import

--- dist/Table.js
+++ dist/Table.js
@@ -1,20 +1,19 @@
-import { Reservations } from "./Reservations.js";
-const reservationForm = document.getElementById('reservationForm');
-const reservationsList = document.getElementById('reservations');
-const errorMessage = document.getElementById('error-message');
-//  code for reservation function needed (based on reservation.ts)
-function updateReservationList() {
-    reservationsList.innerHTML = '';
-    const reservationsArray = Reservations.getAll();
-    reservationsArray.forEach((reservation) => {
-        const listItem = document.createElement('li');
-        listItem.textContent = reservation.toString(); // Assign a string value
-        reservationsList.appendChild(listItem);
+//  import { Reservations } from "./Reservations.js"; 
+const reservationsList = document.getElementById('reservations-list');
+localStorage.getItem("reservationData");
+const reservation = JSON.parse(localStorage.getItem("reservationData") || "[]");
+document.getElementById('reservationForm').onsubmit = function (e) {
+    e.preventDefault();
+    const item = document.createElement('div');
+    const date = document.getElementById("reservation-date").value;
+    const time = document.getElementById("reservation-time").value;
+    const guests = document.getElementById("guests").value;
+    const seating = document.querySelector('input[name="seating"]:checked').value;
+    const table = document.querySelector('input[name="table"]:checked').value;
+    const newReservation = { date, time, guests, seating, table };
+    reservation.forEach(NewRes => {
+        item.innerText = ` ${NewRes.date} at ${NewRes.time} ,  ${NewRes.guests} , ${NewRes.seating} , ${NewRes.table}`;
+        reservationsList.appendChild(item);
+        reservation.push(newReservation);
     });
-}
-function setError(message) {
-    errorMessage.textContent = message;
-}
-function clearError() {
-    errorMessage.textContent = '';
-}
+};
