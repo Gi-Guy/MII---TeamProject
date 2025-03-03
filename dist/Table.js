f3335a842e26c3f3e50de0720656b78ac1679843
Guy <Guy9502@Gmail.com>
2025-03-03 21:25:25 +0200

    .

--- dist/Table.js
+++ dist/Table.js
@@ -1,20 +1,20 @@
-// import { Reservations } from "./Reservations.js";
-// const reservationForm = document.getElementById('reservationForm') as HTMLFormElement;
-// const reservationsList = document.getElementById('reservations') as HTMLUListElement;
-// const errorMessage = document.getElementById('error-message') as HTMLDivElement;
-// //  code for reservation function needed (based on reservation.ts)
-// function updateReservationList() {
-//   reservationsList.innerHTML = '';
-//   const reservationsArray = Reservations.getAll();
-//   reservationsArray.forEach((reservation: any) => {
-//     const listItem = document.createElement('li');
-//     listItem.textContent = reservation.toString(); // Assign a string value
-//     reservationsList.appendChild(listItem);
-//   });
-// }
-// function setError(message: string) {
-//   errorMessage.textContent = message;
-// }
-// function clearError() {
-//   errorMessage.textContent = '';
-// }
+import { Reservations } from "./Reservations.js";
+const reservationForm = document.getElementById('reservationForm');
+const reservationsList = document.getElementById('reservations');
+const errorMessage = document.getElementById('error-message');
+//  code for reservation function needed (based on reservation.ts)
+function updateReservationList() {
+    reservationsList.innerHTML = '';
+    const reservationsArray = Reservations.getAll();
+    reservationsArray.forEach((reservation) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = reservation.toString(); // Assign a string value
+        reservationsList.appendChild(listItem);
+    });
+}
+function setError(message) {
+    errorMessage.textContent = message;
+}
+function clearError() {
+    errorMessage.textContent = '';
+}
