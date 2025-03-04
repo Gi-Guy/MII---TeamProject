e4a1f41055351c9a94c0b3ea2e6c30f206397bc8
Dima Shamis <dims90995@gmail.com>
2025-03-04 21:36:58 +0200

    update user managment

---
+++ backbutton2.ts
@@ -0,0 +1,9 @@
+const goMainSpan3 = document.getElementById("goMain3");
+if (goMainSpan3) {
+  goMainSpan3.addEventListener("click", () => {
+    window.location.href = "MainPage.html";
+  });
+} else {
+  console.error("goMain element not found");
+}
+
